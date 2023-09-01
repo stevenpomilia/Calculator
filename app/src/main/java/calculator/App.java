a40837e7c23b1eb23ac7f47b9220ccd9ba50a522
Steven <stevendetails@gmail.com>
2023-08-31 20:25:58 -0400

    message

--- app/src/main/java/calculator/App.java
+++ app/src/main/java/calculator/App.java
@@ -10,5 +10,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        Calculator c = new Calculator();
+        int answer = c.add(1, 2);
+        System.out.println("1 + 1 = " + answer);
     }
 }
